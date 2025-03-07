dcf5dcbafa7d845232881b8e72b35ee2563bf951
ninjabird <24634476+ninjabird@users.noreply.github.com>
2025-03-07 17:18:30 +0900

    Update auto_stop.sh

--- auto_stop.sh
+++ auto_stop.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 cd /home/ec2-user/minecraftbe/minecraft
 if screen -ls | grep -q "minecraft";
+
 then screen -S minecraft -X stuff "list\015"
 sleep 10
-
 if tail logs/$(ls logs -rt | tail -n 1)  -n 2 | grep -q "There are 0/" ;
+
+sleep 600
+
+then screen -S minecraft -X stuff "list\015"
+sleep 10
+if tail logs/$(ls logs -rt | tail -n 1)  -n 2 | grep -q "There are 0/" ;
+
 then screen -S minecraft -X stuff "stop\015"
 sleep 60
 rm logs/$(ls logs -rt | tail -n 1)|| true
 sudo /sbin/shutdown
+fi
 fi
 fi
