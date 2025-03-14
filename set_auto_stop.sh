01d807ccb762d83f0151fd1720bf69ec0912ac22
ninjabird <24634476+ninjabird@users.noreply.github.com>
2025-03-15 08:50:38 +0900

    Update set_auto_stop.sh

--- set_auto_stop.sh
+++ set_auto_stop.sh
@@ -1,4 +1,9 @@
 curl https://raw.githubusercontent.com/TheRemote/MinecraftBedrockServer/master/SetupMinecraft.sh | bash
+
+sudo chmod +x /home/ec2-user/minecraftbe/minecraft/auto_stop.sh
+sudo chmod +x /home/ec2-user/minecraftbe/minecraft/safe_stop.sh
+sudo chmod +x /home/ec2-user/minecraftbe/minecraft/test.sh
+
 sudo yum install cronie -y
 crobtab -e
 */20 * * * * /home/ec2-user/minecraftbe/minecraft/auto_stop.sh
