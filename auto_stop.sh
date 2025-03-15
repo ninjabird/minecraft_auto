c0314e0f114f344f7d3f906776d5255c48dc4d72
ninjabird <24634476+ninjabird@users.noreply.github.com>
2025-03-15 09:12:41 +0900

    Update auto_stop.sh

--- auto_stop.sh
+++ auto_stop.sh
@@ -12,10 +12,7 @@ then screen -S minecraft -X stuff "list\015"
 sleep 10
 if tail logs/$(ls logs -rt | tail -n 1)  -n 2 | grep -q "There are 0/" ;
 
-then screen -S minecraft -X stuff "stop\015"
-sleep 60
-rm logs/$(ls logs -rt | tail -n 1)|| true
-sudo /sbin/shutdown
+then bash safe_stop.sh
 fi
 fi
 fi
